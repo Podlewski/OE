7978e458c803c7b762ce4262ae231d1f0378dde2
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2020-03-31 13:21:30 +0200

    Sample functions

---
+++ EvoComp/Task1/FunctionsToOptimize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class FunctionsToOptimize
+    {
+        public static double SineTimesCosine(double x1, double x2)
+        {
+            return Math.Sin(x1) * Math.Cos(x2);
+        }
+
+        public static double SineTimesCosinePlusArguments(double x1, double x2)
+        {
+            return SineTimesCosine(x1, x2) + x1 + x2;
+        }
+
+        public static double SineTimesCosinePlusSquareOfArguments(double x1, double x2)
+        {
+            return SineTimesCosine(x1, x2) + x1 * x1 + x2 * x2;
+        }
+
+        public static double Square(float x1)
+        {
+            return x1 * x1;
+        }
+    }
+}
