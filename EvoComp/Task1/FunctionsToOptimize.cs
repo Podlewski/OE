98d47c80cfd7a0b5f0765c3202c7da8e9dcadb3a
Karol Podlewski <podlewski@hotmail.com>
2020-04-01 01:58:08 +0200

    Functions to optimize update

--- EvoComp/Task1/FunctionsToOptimize.cs
+++ EvoComp/Task1/FunctionsToOptimize.cs
@@ -9,19 +9,9 @@ namespace Task1
             return Math.Sin(x1) * Math.Cos(x2);
         }
 
-        public static double SineTimesCosinePlusArguments(double x1, double x2)
+        public static double SineTimesCosineTimesArguments(double x1, double x2)
         {
-            return SineTimesCosine(x1, x2) + x1 + x2;
-        }
-
-        public static double SineTimesCosinePlusSquareOfArguments(double x1, double x2)
-        {
-            return SineTimesCosine(x1, x2) + x1 * x1 + x2 * x2;
-        }
-
-        public static double Multiplication(double x1, double x2)
-        {
-            return x1 * x2;
+            return SineTimesCosine(x1, x2) * x1 - SineTimesCosine(x2, x1) * x2;
         }
 
         public static double Choose(int n, double x1, double x2)
@@ -29,9 +19,7 @@ namespace Task1
             return n switch
             {
                 1 => SineTimesCosine(x1, x2),
-                2 => SineTimesCosinePlusArguments(x1, x2),
-                3 => SineTimesCosinePlusSquareOfArguments(x1, x2),
-                4 => Multiplication(x1, x2),
+                2 => SineTimesCosineTimesArguments(x1, x2),
                 _ => throw new ArgumentException(),
             };
 
