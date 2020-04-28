43842d942b1fdeafa615afb3484abd8533aa1c44
Karol Podlewski <podlewski@hotmail.com>
2020-04-28 14:07:22 +0200

    Added evaluation measures

--- Task2/main.py
+++ Task2/main.py
@@ -1,9 +1,10 @@
 from matplotlib import pyplot as plt
+import numpy as np
 from platypus import ZDT1, ZDT2, ZDT3
 from platypus.algorithms import NSGAII, SPEA2, GDE3
 from platypus.core import nondominated
 from platypus.operators import UniformMutation
-from pymoo.factory import get_problem
+from pymoo.factory import get_problem, get_performance_indicator
 
 from argument_parser import ArgumentParser
 
@@ -37,6 +38,7 @@ par_y = pareto[:, 1]
 res_nod = nondominated(algorithm.result)
 nod_x = [s.objectives[0] for s in res_nod]
 nod_y = [s.objectives[1] for s in res_nod]
+nod = np.array(list(zip(nod_x, nod_y)))
 
 plt.scatter(res_x, res_y, color='red', label='Dominated', s=10)
 plt.scatter(nod_x, nod_y, color='green', label='Nondominated', s=10)
@@ -52,4 +54,16 @@ file_path = type(algorithm).__name__ + '_' + type(test_problem).__name__ + \
             '_g' + str(args.generations) + '_p' + str(args.population) + \
             '_r' + str(args.probability).replace('.', ',') 
 
-plt.savefig(file_path, dpi=300)
+plt.savefig(file_path + '.png', dpi=300)
+
+gd = get_performance_indicator('gd', pareto)
+gd_val = gd.calc(nod)
+
+hv = get_performance_indicator('hv', ref_point=np.array([1, 10]))
+hv_val = hv.calc(nod)
+
+text_file = open(file_path + '.txt', 'wt', encoding='utf-8')
+text_file.write(file_path.replace('_', ' ') + '\n\n')
+text_file.write('GD:\t' + str(round(gd_val, 3)).replace('.', ',') + '\n')
+text_file.write('HV:\t' + str(round(hv_val, 3)).replace('.', ','))
+text_file.close()
